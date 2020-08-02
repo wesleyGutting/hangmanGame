9088f4afa48a33fe9e00741f5ad4eb4897c9d62d
Wesley Gutting <wesleygutting@protonmail.com>
2020-08-02 18:42:42 -0400

    Added rendering of correct and incorrect guesses

--- Hangman.py
+++ Hangman.py
@@ -16,54 +16,57 @@ def checkGuess(list, guess):
             flag = 0
     return flag
 
-def render(wrong, right):
-    clearScreen()
-    print("------------------------------")
+def render(wrong, right, wrongLetter, rightLetter):
+    print("-----------------------------")
     if(wrong >= 1):
-        print("-------------O----------------")
+        print("-------------O---------------")
     else:
-        print("------------------------------")
+        print("-----------------------------")
     if(wrong == 2):
-        print("-------------|----------------")
+        print("-------------|---------------")
     elif(wrong == 3):
-        print("------------\|----------------")
+        print("------------\|---------------")
     elif(wrong >= 4):
-        print("------------\|/---------------")
+        print("------------\|/--------------")
     else:
-        print("------------------------------")
+        print("-----------------------------")
     if(wrong == 5):
-        print("------------/-----------------")
+        print("------------/----------------")
     elif(wrong == 6):
-        print("------------/-\---------------")
+        print("------------/-\--------------")
     else:
-        print("------------------------------")
-    print("------------------------------")
-
+        print("-----------------------------")
+    print("-----------------------------")
+    print(rightLetter)
+    print(wrongLetter)
 
 clearScreen()
-render(6, 0)
+render(6, 0, "", "")
 solution = input("Welcome to the hangman game, please enter the word you would like to play for:\n").upper()
 letters = set(list(solution))
 
 tries = 0
 correctGs = 0
 winner = False
-
+correctLetters = "Correct Guesses:"
+incorrectLetters = "Incorrect Guesses:"
+clearScreen()
 while(tries < 6):
-    #render(tries, correctGs)
+    render(tries, correctGs, correctLetters, incorrectLetters)
     guess = input("Which letter would you like to guess?\n").upper()
-
+    clearScreen()
     flag = checkGuess(letters, guess)
-
     if flag == 1:
         print("Correct!")
         correctGs += 1
+        correctLetters += f" {guess}"
     elif flag == 2:
         print("You already guessed that")
     elif flag ==0:
         print("Incorrect!")
         tries += 1
         print(f"You have {6-tries} errors left")
+        incorrectLetters += f" {guess}"
 
     if(correctGs == len(letters)):
         winner = True
