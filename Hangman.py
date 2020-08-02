94868087ca64fe8a1e4b0c14f6d241874fb853ba
Wesley Gutting <wesleygutting@protonmail.com>
2020-08-02 13:13:59 -0400

    Added some simple rendering, added detection for multiple responses of the same letter

--- Hangman.py
+++ Hangman.py
@@ -1,48 +1,73 @@
 import os
 
+GUESSED_LIST = []
+
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def checkGuess(list, guess):
-    correctList = []
-    flag = 0
     for letter in list:
-        if guess == letter:
-            correctList.append(1)
-            flag = 1
+        if ((guess == letter) and (guess not in GUESSED_LIST)):
+            GUESSED_LIST.append(guess)
+            return 1
+        elif ((guess == letter) and (guess in GUESSED_LIST)):
+            return 2
         else:
-            correctList.append(0)
+            flag = 0
+    return flag
 
-    return flag, correctList
-
-def render():
+def render(wrong, right):
     clearScreen()
-    print("Bruh")
+    print("------------------------------")
+    if(wrong >= 1):
+        print("-------------O----------------")
+    else:
+        print("------------------------------")
+    if(wrong == 2):
+        print("-------------|----------------")
+    elif(wrong == 3):
+        print("------------\|----------------")
+    elif(wrong >= 4):
+        print("------------\|/---------------")
+    else:
+        print("------------------------------")
+    if(wrong == 5):
+        print("------------/-----------------")
+    elif(wrong == 6):
+        print("------------/-\---------------")
+    else:
+        print("------------------------------")
+    print("------------------------------")
+
 
 clearScreen()
-solution = input("Welcome to the hangman game, please enter the word you would like to play for:\n\n").upper()
+render(6, 0)
+solution = input("Welcome to the hangman game, please enter the word you would like to play for:\n").upper()
 letters = set(list(solution))
 
 tries = 0
 correctGs = 0
+winner = False
 
 while(tries < 6):
+    #render(tries, correctGs)
     guess = input("Which letter would you like to guess?\n").upper()
 
-    flag, check = checkGuess(letters, guess)
+    flag = checkGuess(letters, guess)
 
     if flag == 1:
         print("Correct!")
         correctGs += 1
-    else:
+    elif flag == 2:
+        print("You already guessed that")
+    elif flag ==0:
         print("Incorrect!")
         tries += 1
+        print(f"You have {6-tries} errors left")
 
     if(correctGs == len(letters)):
         winner = True
         break
-
-    print(f"You have {5-tries} tries left")
 
 clearScreen()
 if(winner == True):
